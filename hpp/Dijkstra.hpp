0b70f1f0db130bb2962588769e89489ba4794368
Rascaleuh <etienne.idoux@gmail.com>
2019-01-12 02:34:00 +0100

    Début du debug de Dijkstra

--- hpp/Dijkstra.hpp
+++ hpp/Dijkstra.hpp
@@ -12,7 +12,7 @@ void Dijkstra(Graph<I,int>* graph, Noeud<I,int>* src){
 
 	int INFINITY = 100000;
 
-
+	std::cout << "***** INIT *****" << std::endl;
 	std::map<Noeud<I,int>*, int> result = std::map<Noeud<I,int>*,int>();
 
 	std::map<Noeud<I,int>*, std::tr1::tuple<Noeud<I,int>*, int>*> tmp = std::map<Noeud<I,int>*, std::tr1::tuple<Noeud<I,int>*, int>*>();
@@ -34,24 +34,50 @@ void Dijkstra(Graph<I,int>* graph, Noeud<I,int>* src){
 		}
 	}
 
-	
-
+	std::cout << "***** BOUCLE *****" << std::endl;
 	std::tr1::tuple<Noeud<I,int>*,int>* choice;
+
 	while(!tas_id.isEmpty()){
+		std::cout << "****Traitement de" << std::endl;
 		choice = tas_id.outMin();
-		result[std::tr1::get<0>(choice)] = std::tr1::get<1>(choice);
-		for (typename std::vector<std::tr1::tuple<Noeud<I,int>*, int> >::iterator it = std::tr1::get<0>(choice)->getNeighbours().begin(); it != std::tr1::get<0>(choice)->getNeighbours().end(); it++){
-			std::tr1::tuple<Noeud<I,int>*, int>* curTuple = tmp[*it.first];
-			if (curTuple->second > *it->second + std::tr1::get<1>(choice)){
-				curTuple->second = *it.second + std::tr1::get<1>(choice);
+
+		Noeud<I, int>* noeudMin = std::tr1::get<0>(*choice);
+		int poidMin = std::tr1::get<1>(*choice);
+
+		std::cout << *noeudMin << std::endl;
+		
+		result[noeudMin] = poidMin;
+		
+		std::vector< std::tr1::tuple<Noeud<I,int>*, int> > voisinsMin = noeudMin->getNeighbours();
+
+		std::cout << "***Traitement des voisins" << std::endl;
+		for( typename std::vector< std::tr1::tuple<Noeud<I,int>*, int> >::iterator it = voisinsMin.begin(); it != voisinsMin.end(); it++){
+			std::cout << "**Traitement du voisin" << std::endl;
+			Noeud<I, int>* noeudVoisin = std::tr1::get<0>(*it);
+			std::cout << *noeudVoisin << std::endl;
+
+			std::tr1::tuple<Noeud<I,int>*, int>* tupleVoisin = tmp[std::tr1::get<0>(*it)];
+
+			if (std::tr1::get<1>(*tupleVoisin) > std::tr1::get<1>(*it) + std::tr1::get<1>(*choice)){
+				std::tr1::get<1>(*tupleVoisin) = std::tr1::get<1>(*it) + std::tr1::get<1>(*choice);
+				tas_id.updateTas(tupleVoisin);
+				std::cout << "**On change la valeur de ce voisin" << std::endl;
+			}
+
+		}
+		/*for (typename std::vector<std::tr1::tuple<Noeud<I,int>*, int> >::iterator it = (std::tr1::get<0>(*choice))->getNeighbours().begin(); it != std::tr1::get<0>(*choice)->getNeighbours().end(); it++){
+			std::cout << "IN FOR getNeighbours" << std::endl;
+			std::tr1::tuple<Noeud<I,int>*, int>* curTuple = tmp[std::tr1::get<0>(*it)];
+			if (std::tr1::get<1>(*curTuple) > std::tr1::get<1>(*it) + std::tr1::get<1>(*choice)){
+				std::tr1::get<1>(*curTuple) = std::tr1::get<1>(*it) + std::tr1::get<1>(*choice);
 				tas_id.updateTas(curTuple);
 			}
-		}
+		}*/
 	}
 
-	for (typename std::map<Noeud<I,int>*, int>::iterator it = result.begin(); it != result.end(); it++){
+	/*for (typename std::map<Noeud<I,int>*, int>::iterator it = result.begin(); it != result.end(); it++){
 		std::cout << *(std::tr1::get<0>(*it)) << " : " << *(std::tr1::get<1>(*it)) << std::endl;
-	}
+	}*/
 }
 
 
